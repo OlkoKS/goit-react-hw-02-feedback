8b16bdbda6541e21e22b7bbdc65eef2df728fd86
OlkoKS <126245735+OlkoKS@users.noreply.github.com>
2023-09-09 10:54:09 +0200

    task-feedback

---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types';
+import { BtnContainer, BtnElement } from '../FeedbackOptions/FeedbackOptions.styled';
+
+export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+    return (
+        <BtnContainer>
+            {options.map((option) => {
+                return <BtnElement type="button" onClick={onLeaveFeedback} key={option}>{option}</BtnElement>
+            })}
+        </BtnContainer>
+    )
+}
+
+FeedbackOptions.propTypes = {
+    options: PropTypes.array.isRequired,
+    onLeaveFeedback: PropTypes.func.isRequired,
+}
